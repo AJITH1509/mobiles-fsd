1164f9d8aead9328d31916b18f575c1c11aa3e52
AJITH1509 <ajithmuthu799@gmail.com>
2023-03-04 18:01:57 +0530

    salt round changed

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/addmobiles", async function (request, response) {
 
 // function for hashing password
 async function generateHashedPassword(password) {
-  const NO_OF_ROUNDS = 1;
+  const NO_OF_ROUNDS = 10;
   const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
