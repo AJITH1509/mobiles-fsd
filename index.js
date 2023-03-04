8c48e9c4caf76afae6bf08179de044308a7f9aeb
AJITH1509 <ajithmuthu799@gmail.com>
2023-03-04 18:00:33 +0530

    login signup endpoint created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -33,5 +35,75 @@ app.post("/addmobiles", async function (request, response) {
     .insertMany(data);
   response.send(result);
 });
+
+// function for hashing password
+async function generateHashedPassword(password) {
+  const NO_OF_ROUNDS = 1;
+  const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+app.post("/signup", express.json(), async function (request, response) {
+  try {
+    const { username, password } = request.body;
+    const userFromDb = await getUserByName(username);
+    // check username exists
+    if (userFromDb) {
+      response.status(401).send({ message: "username already exists" });
+    }
+    //set password length for security purposes
+    else if (password.length < 8) {
+      response
+        .status(400)
+        .send({ message: "Password must be at least 8 characters" });
+    }
+    //all condition passed allowed add user with hash value
+    else {
+      const hashedPassword = await generateHashedPassword(password);
+      const result = await addUser({
+        username: username,
+        password: hashedPassword,
+      });
+
+      response.send(result);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async function (request, response) {
+  try {
+    const { username, password } = request.body;
+    const userFromDb = await getUserByName(username);
+    // check username exists
+    if (!userFromDb) {
+      response.status(401).send({ message: "invalid credentials" });
+    } else {
+      const storedPassword = userFromDb.password;
+      const isPasswordCheck = await bcrypt.compare(password, storedPassword);
+      if (isPasswordCheck) {
+        const token = jwt.sign({ id: userFromDb._id }, process.env.Secret_Key);
+        response.send({ message: "login successfull", token: token });
+      } else {
+        response.status(401).send({ message: "invalid credentials" });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+async function addUser(data) {
+  return await client.db("b42wd2").collection("users").insertOne(data);
+}
+
+async function getUserByName(username) {
+  return await client
+    .db("b42wd2")
+    .collection("users")
+    .findOne({ username: username });
+}
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
